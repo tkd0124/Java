1573c6a6da90cc267000b45183f9eb65ee82c6fd
tkd0124 <188334535+tkd0124@users.noreply.github.com>
2024-12-29 15:08:37 +0900

    練習２

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ class C_2_04_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        System.out.println(2 * N + 3);
+        System.out.println(5 * N + 3);
     }
 }
---
+++ Main2.java
@@ -0,0 +1,7 @@
+class C_2_04_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(4 * N + 3);
+    }
+}
