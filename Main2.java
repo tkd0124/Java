efaa6764d8191e6de99a7446f041d071de250300
tkd0124 <188334535+tkd0124@users.noreply.github.com>
2024-12-29 16:19:09 +0900

    線形時間

--- Main2.java
+++ Main2.java
@@ -1,7 +1,19 @@
-class C_2_04_1 {
+import java.util.*;
+
+class liner_time {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        System.out.println(4 * N + 3);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+
+        int cnt = 0;
+        for (int i = 1; i <= N; i++) {
+            if (i % X == 0 || i % Y == 0){
+                    cnt++;
+            } 
+        }
+
+        System.out.println(cnt);
     }
 }
