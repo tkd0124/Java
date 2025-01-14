95a213caae584109af54ac8b5443dc6f0dbfc5d0
tkd0124 <188334535+tkd0124@users.noreply.github.com>
2025-01-14 21:12:23 +0900

    j_b3

---
+++ r3_7.java
@@ -0,0 +1,8 @@
+public class r3_7 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.print("現在" + (i + 1) + "周目→");
+        }
+    }
+    
+}
