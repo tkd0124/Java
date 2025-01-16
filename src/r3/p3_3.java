0ccf669dc0199b024c88db2f54b4e44caf22bef3
tkd0124 <188334535+tkd0124@users.noreply.github.com>
2025-01-16 23:44:37 +0900

    jb3

---
+++ src/r3/p3_3.java
@@ -0,0 +1,18 @@
+package r3;
+
+public class p3_3 {
+    public static void main(String[] args) {
+        // int isHungry = 0;
+        int isHungry = 1;
+        String food = "m";
+        System.out.println("Hellow");
+        if (isHungry == 0 ) {
+            System.out.println("お腹いっぱい");
+        } else {
+            System.out.println("ぺこ");
+            System.out.println(food +"をいただきます");
+            
+        }
+        System.out.println("ごちそうさまです");
+    }
+}
