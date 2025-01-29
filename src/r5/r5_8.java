abf2c2ea921c38f3b7fe044319dae45c3a1d13c3
tkd0124 <188334535+tkd0124@users.noreply.github.com>
2025-01-29 23:47:16 +0900

    jb5

--- src/r5/r5_8.java
+++ src/r5/r5_8.java
@@ -1,14 +1,11 @@
 package r5;
 
-public class r5_7 {
+public class r5_8 {
     public static int add(int x, int y) {
         int ans = x + y;
         return ans;
       }
       public static void main(String[] args) {
-        int ans = add(100, 10);
-         System.out.println("100 + 10 = " + ans);
-        //System.out.println( x + "+" + y + "=" + ans);
+        System.out.println(add(add(10, 20), add(30, 40)));
       }
-    
 }
