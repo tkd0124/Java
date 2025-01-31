fedca9e87abbe555aa18f59d5efa33a2addeb589
tkd0124 <188334535+tkd0124@users.noreply.github.com>
2025-01-31 14:36:34 +0900

    jb5

---
+++ src/r5/j5_11.java
@@ -0,0 +1,12 @@
+public class j5_11 {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+    public static int add(int x, int y, int z) {
+        return x + y + z;
+    }
+    public static void main(String[] args){
+        System.out.println("10+20=" + add(10, 20));
+        System.out.println("10+20+30=" + add(10, 20, 30));
+    }
+}
