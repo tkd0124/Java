265e8c3ca4ace83c4765e84cac468327ffc74226
tkd0124 <188334535+tkd0124@users.noreply.github.com>
2025-02-05 23:40:51 +0900

    jb9

--- Hero.java
+++ Hero.java
@@ -1,25 +1,34 @@
 public class Hero {
     String name;
     int hp;
-//    public void attack() {/* … */}
-    public void sleep() {
-        this.hp = 300;
-        System.out.println(this.name + "は、眠って" + this.hp + "回復した！");
-        }
-    public void sit(int sec) {
-        this.hp += sec;
-        System.out.println(this.name + "は、" + sec + "秒座った！");
-        System.out.println("HPが" + sec + "ポイント回復した");
+    Sword sword;
+    public void attack(){
+        System.out.println(this.name + "は攻撃した！");
+        System.out.println("敵に5ポイントのダメージをあたえた！");
     }
-    public void slip() {
-        this.hp -= 5;
-        System.out.println(this.name + "は、転んだ！");
-        System.out.println("5のダメージ！");
+    public Hero(String name){
+        this.hp = 100;  //hpフィールドを100で初期化
+        this.name = name;   //// 引数の値でnameフィールドを初期化
     }
-    public void run() {
-        System.out.println(this.name + "は、逃げ出した！");
-        System.out.println("GAMEOVER");
-        System.out.println("最終HPは" + this.hp + "でした");
-    }
+////    public void attack() {/* … */}
+//    public void sleep() {
+//        this.hp = 300;
+//        System.out.println(this.name + "は、眠って" + this.hp + "回復した！");
+//        }
+//    public void sit(int sec) {
+//        this.hp += sec;
+//        System.out.println(this.name + "は、" + sec + "秒座った！");
+//        System.out.println("HPが" + sec + "ポイント回復した");
+//    }
+//    public void slip() {
+//        this.hp -= 5;
+//        System.out.println(this.name + "は、転んだ！");
+//        System.out.println("5のダメージ！");
+//    }
+//    public void run() {
+//        System.out.println(this.name + "は、逃げ出した！");
+//        System.out.println("GAMEOVER");
+//        System.out.println("最終HPは" + this.hp + "でした");
+//    }
 }
 
