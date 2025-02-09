bdd494040f4d52e8d9930561b11f15270e881854
tkd0124 <188334535+tkd0124@users.noreply.github.com>
2025-02-09 21:30:20 +0900

    kadai3

--- src/k3_5.java
+++ src/k3_5.java
@@ -3,7 +3,7 @@ package src;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class k3_5{
+public class k3_5 {
     public static void main(String[] args) {
         String[][] array = {
                 {"tanaka", "tpass"},
@@ -11,30 +11,56 @@ public class k3_5{
                 {"sato", "spass"},
         };
         System.out.println("accounts = " + Arrays.deepToString(array));
-
         System.out.println("ユーザ名とパスワードを入力してください。");
 
         Scanner scanner = new Scanner(System.in);
 
-
-
         while (true) {
             System.out.print("ユーザ名：");
-            String y = scanner.next();
+            String y = scanner.nextLine();
             System.out.print("パスワード：");
-            String p = scanner.next();
-            for (int i = 0; i < array.length; i++) {
-                if (array[i][0].equals("y") && array[i][1].equals("p")) {
-                    System.out.println("ログイン成功");
+            String p = scanner.nextLine();
+
+            boolean isLoggedIn = false;
+            for (String[] account : array){
+                String username = account[0];
+                String password = account[1];
+                if (username.equals(y) && password.equals(p)){
+                    isLoggedIn = true;
                     break;
-                } else {
-                    System.out.println("ログイン失敗");
-
                 }
-                break;
             }
+
+            if (isLoggedIn){
+                System.out.println("ログイン成功");
+                break;
+            }else {
+                System.out.println("ログイン失敗");
+            }
+//            String p = scanner.nextLine();
+
+//            boolean ty = false;
+//            boolean tp = false;
+
+//            for (String[] i : array) {
+//
+//                for (String j : i) {
+//                    if (j.equals(y)) {
+//                        ty = true;
+//                    }
+//                    if (j.equals(p)) {
+//                        tp = true;
+//                    }
+//                    if (ty && tp) {
+//                        System.out.println("ログイン成功");
+//                        t = false;
+//                        break;
+//                    }
+//                }
+//            }
+//            System.out.println("ログイン失敗");
         }
-
-
+        scanner.close();
     }
+
 }
