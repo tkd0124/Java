ac36fe81320b0f5150c6f57bed478665b2c791bf
tkd0124 <188334535+tkd0124@users.noreply.github.com>
2025-02-12 14:26:54 +0900

    product

---
+++ src/r10/j10/SuperHero.java
@@ -0,0 +1,28 @@
+package j10;
+
+public class SuperHero extends Hero{
+    public SuperHero() {
+        super();
+        System.out.println("SuperHeroのコンストラクタが動作");
+    }
+
+    boolean flying;
+    public void fly() {
+        this.flying = true;
+        System.out.println("飛び上がった！");
+    }
+    public void land() {
+        this.flying = false;
+        System.out.println("着地した！");
+    }
+    @Override
+    public void run() {
+        System.out.println(this.name + "　は　" + "撤退した。");
+    }
+    public void attack(Matango m){
+        super.attack(m);
+        if (this.flying){
+            super.attack(m);
+        }
+    }
+}
---
+++ src/r10/j10/Weapon.java
@@ -0,0 +1,8 @@
+package j10;
+
+public class Weapon extends Item{
+    public Weapon(){
+        super("momo");
+//        super("poyopp", 89);
+    }
+}
